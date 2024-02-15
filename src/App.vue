3dedd09252fb62a7146430f4766e802012f9a3f2
BAALLOU Hicham <baallou1996h@gmail.com>
2024-02-15 10:08:08 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import Sidebar from "./components/Sidebar.vue";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <Sidebar />
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  font-family: "Fira sans", sans-serif;
+}
+
+.app {
+  display: flex;
+
+  main {
+    flex: 1 1 0;
+    padding: 2rem;
+
+    @media (max-width: 1024px) {
+      padding-left: 6rem;
+    }
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <main>
+    <div class="home">
+      <h1>This is an home page</h1>
+    </div>
+  </main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .home {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
